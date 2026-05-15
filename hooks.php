be422fc01583d24cc8f4e2e617a1755f23c302e2
Kevin Fraser <kevin@ksfraser.com>
2026-05-15 08:00:47 -0600

    fix: add version property and composer install on activation

--- hooks.php
+++ hooks.php
@@ -7,6 +7,7 @@ define('SS_TEAMS', 137 << 8);
 
 class hooks_fa_teams extends hooks {
     var $module_name = 'fa_teams';
+    var $version = '1.0.0';
 
     function install_options($app) {
         global $path_to_root;
